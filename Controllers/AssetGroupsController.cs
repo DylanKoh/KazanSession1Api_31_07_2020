d36fed58ab12e1f82cb3fbe8fc51bdd6b023d755
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-07-31 15:41:59 +0800

    Added controllers for client side to call

---
+++ Controllers/AssetGroupsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KazanSession1Api_31_07_2020;
+
+namespace KazanSession1Api_31_07_2020.Controllers
+{
+    public class AssetGroupsController : Controller
+    {
+        private Session1Entities db = new Session1Entities();
+
+        public AssetGroupsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // POST: AssetGroups
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return Json(db.AssetGroups.ToList());
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KazanSession1Api_31_07_2020;
+
+namespace KazanSession1Api_31_07_2020.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private Session1Entities db = new Session1Entities();
+
+        public DepartmentsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // POST: Departments
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return Json(db.Departments.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
